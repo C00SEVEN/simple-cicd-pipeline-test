099dd1d7b71c8e2af11697f059d08453b7f743f1
Carlos Almeida <c_cesar007@hotmail.com>
2026-08-04 23:03:02 +0100

    Add NSG associations for subnets

--- main.tf
+++ main.tf
@@ -1,48 +1,49 @@
 resource "azurerm_resource_group" "this" {
-
-  name = var.resource_group_name
-
+  name     = var.resource_group_name
   location = var.location
-
 }
-
-
 
 resource "azurerm_virtual_network" "this" {
-
-  name = var.vnet_name
-
-  location = azurerm_resource_group.this.location
-
+  name                = var.vnet_name
+  location            = azurerm_resource_group.this.location
   resource_group_name = azurerm_resource_group.this.name
-
-  address_space = var.address_space
-
+  address_space       = var.address_space
 }
 
-
-
 resource "azurerm_subnet" "this" {
-
-  name = var.subnet_name
-
-  resource_group_name = azurerm_resource_group.this.name
-
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.this.name
   virtual_network_name = azurerm_virtual_network.this.name
-
-  address_prefixes = var.subnet_prefix
+  address_prefixes     = var.subnet_prefix
 
 }
 
 resource "azurerm_subnet" "this2" {
-
-  name = var.subnet_name2
-
-  resource_group_name = azurerm_resource_group.this.name
-
+  name                 = var.subnet_name2
+  resource_group_name  = azurerm_resource_group.this.name
   virtual_network_name = azurerm_virtual_network.this.name
-
-  address_prefixes = var.subnet_prefix2
-
+  address_prefixes     = var.subnet_prefix2
 }
 
+resource "azurerm_network_security_group" "workload" {
+  name                = "nsg-workloads"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+}
+
+
+resource "azurerm_network_security_group" "workload2" {
+  name                = "nsg-workloads2"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+}
+
+resource "azurerm_subnet_network_security_group_association" "subnet1" {
+  subnet_id                 = azurerm_subnet.this.id
+  network_security_group_id = azurerm_network_security_group.workload.id
+}
+
+resource "azurerm_subnet_network_security_group_association" "subnet2" {
+  subnet_id                 = azurerm_subnet.this2.id
+  network_security_group_id = azurerm_network_security_group.workload2.id
+} 
